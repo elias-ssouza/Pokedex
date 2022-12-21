cca6f4440c3b7f369ced8b7a530d477dc3cdde24
elias-ssouza <elias.ssouza@hotmail.com>
2022-12-21 17:48:13 -0300

    finalização do PokemonAdapter.kt

--- app/src/main/java/com/darkgreen/pokedex/View/MainActivity.kt
+++ app/src/main/java/com/darkgreen/pokedex/View/MainActivity.kt
@@ -2,11 +2,33 @@ package com.darkgreen.pokedex.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.darkgreen.pokedex.Domain.Pokemon
+import com.darkgreen.pokedex.Domain.PokemonType
 import com.darkgreen.pokedex.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_pokemon)
+
+        val layoutManager = LinearLayoutManager(this)
+
+        val pokemons = listOf(Pokemon(
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+            1,
+            "Pikachu",
+            listOf(
+                PokemonType("Eletric")
+            )
+        )
+        )
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
--- app/src/main/java/com/darkgreen/pokedex/View/PokemonAdapter.kt
+++ app/src/main/java/com/darkgreen/pokedex/View/PokemonAdapter.kt
@@ -3,12 +3,14 @@ package com.darkgreen.pokedex.View
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.darkgreen.pokedex.Domain.Pokemon
 import com.darkgreen.pokedex.R
 
 class PokemonAdapter (
-    val items: List<Pokemon>
+    private val items: List<Pokemon>
 ): RecyclerView.Adapter<PokemonAdapter.ViewHolder> (){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -18,13 +20,33 @@ class PokemonAdapter (
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = items[position]
+
+        holder.bindView(item)
     }
 
     override fun getItemCount() =  items.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindView(){
+        fun bindView(item: Pokemon) = with(itemView){
+            val ivPokemon = findViewById<ImageView>(R.id.iv_pokemon)
+            val tvNumber = findViewById<TextView>(R.id.tv_number)
+            val tvName = findViewById<TextView>(R.id.tv_name)
+            val tvPokemonType1 = findViewById<TextView>(R.id.tv_pokemon_type1)
+            val tvPokemonType2 = findViewById<TextView>(R.id.tv_pokemon_type2)
+
+            //TODO: Glide
+
+            tvNumber.text = "Nº ${item.number}"
+            tvName.text = item.name
+            tvPokemonType1.text = item.types[0].name
+
+            if (item.types.size >1) {
+                tvPokemonType2.visibility = View.VISIBLE
+                tvPokemonType2.text = item.types [1].name
+            } else {
+                tvPokemonType2.visibility = View.GONE
+            }
 
         }
     }
